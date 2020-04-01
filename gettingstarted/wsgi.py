6f9ddeecca5929c74ba09de459ebeb9c06f7a1d9
sjs1178 <sjs1178@gmail.com>
2020-04-02 00:00:32 +0900

    Update wsgi.py

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -13,4 +13,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+def hello():
+  return "Hello"
+
+application = hello()
